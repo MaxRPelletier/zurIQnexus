40316a649f6fb8310afa624a529abad7ef025243
aloha-fim <frederec.im@gmail.com>
2025-04-12 01:04:40 +0200

    frontend setup and config to tailwind css

---
+++ AI_chatbot/frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
